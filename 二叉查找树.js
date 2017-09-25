9c254f6da4d1df6056ad998b2bce3a94fe3cd26d
mayuan <xiaoma961211@163.com>
2017-09-25 10:55:51 +0800

    二叉查找树
    二叉查找树

--- 二叉查找树.js
+++ 二叉查找树.js
@@ -1,5 +1,9 @@
 //二叉查找树（二叉顺序树、二叉搜素树）
-
+/**
+(1) 查找给定值；
+(2) 查找最小值；
+(3) 查找最大值。
+**/
 function BST() {
   this.root = null;
   this.insert = insert;
@@ -33,4 +37,34 @@ function insert(data) {
       }
     }
   }
+}
+//查找最小值
+function getMin() {
+  var current = this.root;
+  while (!(current.left == null)) {
+    current = current.left;
+  }
+  return current.data;
+}
+//查找最大值
+function getMax() {
+  var current = this.root;
+  while (!(current.right == null)) {
+    current = current.right;
+  }
+  return current.data;
+}
+//查找给定值
+function find(data) {
+  var current = this.root;
+  while (current != null) {
+    if (current.data == data) {
+     return current;
+    }else if (data < current.data) {
+      current = current.left;
+    } else {
+      current = current.right;
+   }
+  }
+  return null;
 }
