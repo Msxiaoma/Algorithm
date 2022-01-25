273783de7fcedb6a1bbaa466baadcc5c95fbe2dd
maai <xiaoma961211@163.com>
2022-01-25 17:54:32 +0800

    继承

--- test.js
+++ test.js
@@ -29,3 +29,16 @@ setTimeout(() => {
 // 'timer1'
 
 
+
+https://account.xueersi.com;
+https://activity.xueersi.com;
+https://api.xueersi.com;
+https://app.xueersi.com;
+https://booster.xueersi.com;
+https://chatgslb.xescdn.com;
+https://chatgslb.xesimg.com;
+https://dj.xesimg.com;
+https://editor.xesimg.com;
+https://expapi.xueersi.com;https://growth.xueersi.com;https://gslb.video.xescdn.com;https://gslb.xueersi.com;https://gslbbak.xueersi.com;https://h5.xueersi.com;https://icourse.xesimg.com;https://lecturepie.xueersi.com;https://live.xueersi.com;https://log.xescdn.com;https://login.xueersi.com;https://m.xueersi.com;https://ocenter.xueersi.com;https://open.weixin.qq.com;https://passport.100tal.com;https://passport.vdyoo.com;https://pv.sohu.com;https://reg.xueersi.com;https://replayws.wangxiao.eaydu.com;https://test-passport.100tal.com;https://test-passport.vdyoo.com;https://test-ucdj.100tal.com;https://test-udcdj.100tal.com;https://time.xueersi.com;https://touch.xueersi.com;https://trade.xueersi.com;https://ucdj.100tal.com;https://udcdj.100tal.com;https://weixin.xueersi.com;https://www.xueersi.com;https://xesfile.xesimg.com;
+
+
